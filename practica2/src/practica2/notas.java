e89b14e035b4d8d157607c5a7d922812c2e4fce3
Ana <anittarsc@gmail.com>
2023-04-02 13:52:32 +0200

    Exportar github

--- practica2/src/practica2/notas.java
+++ practica2/src/practica2/notas.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class notas {
 	//declaramos la variables que nos hacen falta
-	double uf1, uf2, uf3;
+	double notasUF1, notasUF2, notasUF3;
 	double acu1, acu2, acu3, def;
 	//utilizames ecaner para poder introducir datos
 	Scanner entrada = new Scanner(System.in);
@@ -17,16 +17,16 @@ public class notas {
 		
 		System.out.print("ingrese nota 1: ");
 		
-		uf1= entrada.nextDouble();
+		notasUF1= entrada.nextDouble();
 
 		
 		
 		System.out.print("ingrese nota 2: ");
 		
-		uf2= entrada.nextDouble();
+		notasUF2= entrada.nextDouble();
 		System.out.print("ingrese nota 3: ");
 		
-		uf3= entrada.nextDouble();
+		notasUF3= entrada.nextDouble();
 		
 	
 	}
@@ -35,20 +35,20 @@ public class notas {
 	
 	public void comprobarcion(){
 		
-		if (uf1>10) {
+		if (notasUF1>10) {
 			System.out.println(" nota1 mal introducida");
 			
 		}else {
 			System.out.println(" nota1 correcta");
 		}
 		
-		if (uf2>10) {
+		if (notasUF2>10) {
 			System.out.println(" nota2 mal introducida");
 			
 		}else {
 			System.out.println(" nota2 correcta");
 		}
-		if (uf3>10) {
+		if (notasUF3>10) {
 			System.out.println(" nota3 mal introducida");
 			
 		}else {
@@ -62,9 +62,9 @@ public class notas {
 	// metodo para calcular nota
 	
 	public void Calculonotas() {
-		acu1= uf1*0.35;
-		acu2 = uf2 * 0.35;
-		acu3 = uf3 * 0.30;
+		acu1= notasUF1*0.35;
+		acu2 = notasUF2 * 0.35;
+		acu3 = notasUF3 * 0.30;
 		
 		def = acu1 + acu2+ acu3;
 		
@@ -73,9 +73,9 @@ public class notas {
 	public void Mostrar() {
 		//print ln lo que hace es que al terminar el print baja la linea
 		System.out.println(" notas introducidas son:");
-		System.out.println(" nota1 = " + uf1);
-		System.out.println(" nota2 = " + uf2);
-		System.out.println(" nota3 = " + uf3);
+		System.out.println(" nota1 = " + notasUF1);
+		System.out.println(" nota2 = " + notasUF2);
+		System.out.println(" nota3 = " + notasUF3);
 		
 		System.out.println(" acumuado 1 = "+ acu1);
 		System.out.println(" acumuado 2 = "+ acu2);
